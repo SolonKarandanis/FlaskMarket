cf82ba9392dfd09a393bf7d076a95d041df8e1f1
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-04-15 09:16:06 +0300

    Added logs for quieries

--- market/__init__.py
+++ market/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://sales_db:sales_db@192.168.1.5:5432/sales_db"
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager= LoginManager(app)
