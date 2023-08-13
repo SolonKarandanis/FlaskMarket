5a217803dd8d3e9d3dd4564a17c3df9c94b58d46
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-08-13 10:25:43 +0300

    Created Config class

--- market/__init__.py
+++ market/__init__.py
@@ -4,14 +4,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from market.config import Config
 
 app = Flask(__name__)
 logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://sales_db:sales_db@192.168.1.5:5432/sales_db"
-app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+app.config.from_object(Config)
 app.jinja_env.globals.update(zip=zip)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
