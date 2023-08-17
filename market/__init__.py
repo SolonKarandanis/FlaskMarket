6e738946908de4c04c08acf40585b7934c2429dd
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2023-08-17 13:35:52 +0300

    Logging to a File

--- market/__init__.py
+++ market/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from market.config import Config
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s')
@@ -19,3 +21,15 @@ login_manager.login_view = 'login_page'
 login_manager.login_message_category = 'info'
 from market import routes,errors
 
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/flask_market.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
